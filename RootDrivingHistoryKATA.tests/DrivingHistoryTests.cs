487dbccaea2848784dd3e4e532df16185611f4f9
Peter Kirk <codesmore@yahoo.com>
2019-02-08 09:24:36 -0500

    Test002_GetOutputData red

--- RootDrivingHistoryKATA.tests/DrivingHistoryTests.cs
+++ RootDrivingHistoryKATA.tests/DrivingHistoryTests.cs
@@ -14,5 +14,15 @@ namespace RootDrivingHistoryKATA.tests
 
             Assert.AreEqual("Driver Peter Trip Peter 13:15 13:45 17.3", input);
         }
+
+        [TestMethod]
+        public void Test002_GetOutputData()
+        {
+            DrivingHistory drivingHistory = new DrivingHistory();
+
+            string input = drivingHistory.GetInputData("TestData001.txt");
+
+            Assert.AreEqual("Peter: 17 miles @ 37 mph", drivingHistory.ToString());
+        }
     }
 }
